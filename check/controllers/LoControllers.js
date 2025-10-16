e8fbae0d10f54b9fe094642b7ad18c44aaeab2b7
devvisibiliti <dev@visibiliti.in>
2025-10-16 18:28:56 +0530

    login updated

--- check/controllers/LoControllers.js
+++ check/controllers/LoControllers.js
@@ -1,31 +1,39 @@
 import User from "../models/userModels.js"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const Lo = async (req, res)=>{
     try{
         const {email, password} = req.body
     if(!email || !password){
-        return res.status(400).JSON({message:"email or password is not there"})
+        return res.status(400).json({message:"email or password is not there"})
     }
-    const log = await User.findOne({email:email.toLowercase().trim()}).select("+password")
+    const log = await User.findOne({email:email.toLowerCase().trim()}).select("+password")
     if(!log){
-        return res.status(401).JSON({message:"user not there"})
+        return res.status(401).json({message:"user not there"})
     }
-    const ok = await bcrypt.compare(password, User.log )
+    const ok = await bcrypt.compare(password, log.password )
     if(!ok){
-        return res.status(501).JSON({message:"password is wrong"})
+        return res.status(501).json({message:"password is wrong"})
     }
-
     const token = jwt.sign({
         sub:log._id, username: log.username, email: log.email
     },
     process.env.JWT_SECRET,
-    {expiresin:"7d"}
+    {expiresIn:"7d"}
 );
 
-return res.status(200).JSON({
+res.cookie("auth", token, {
+    httpOnly:true,
+    secure:process.env.NODE_ENV ==="production",
+    sameSite:process.env.NODE_ENV === "production"?"none":"lax",
+    maxAge:7*24*60*60*1000,
+    path:'/'
+
+})
+
+return res.status(200).json({
     message:"login successfull",
-    token,
     log: { id: log._id, username: log.username, email: log.email }
 })
 
--- check/index.js
+++ check/index.js
@@ -37,3 +37,4 @@ app.use("/api", router)
 app.use("/ad", adRouter)
 
 
+
