b0eccc280d3fbd810b8952f1c092b42d171280ce
devvisibiliti <dev@visibiliti.in>
2025-12-11 16:11:52 +0530

    pro changed

--- check/routes/products.js
+++ check/routes/products.js
@@ -1,6 +1,6 @@
 import express from "express";
 import slugify from "slugify";
-import Product from "../models/product.js";
+import Product from "../models/products.js";
 
 const router = express.Router();
 
